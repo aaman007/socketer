2e600b66bf1c01eb846829c089fd81dd31f1fe4b
Amanur Rahman <aaman.sunny007@gmail.com>
2022-02-12 20:50:08 +0600

    Fix import order

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require("socket.io");
+
+const server = http.createServer(app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
